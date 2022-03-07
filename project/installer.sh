f2d2d66f407b289df4df10187f1be59b1582ea47
Aditya Oli <adityaoligehu@gmail.com>
2022-03-07 13:13:10 +1300

    push

---
+++ project/installer.sh
@@ -0,0 +1,15 @@
+export VERIFY_CHECKSUM=false
+curl -sSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+wget https://releases.hashicorp.com/terraform/1.0.7/terraform_1.0.7_linux_amd64.zip
+unzip terraform_1.0.7_linux_amd64.zip
+mkdir ~/bin
+mv terraform ~/bin
+export TF_PLUGIN_CACHE_DIR="/tmp"
+curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+kubectl create namespace monitoring
+kubectl create secret generic additional-scrape-configs --from-file=prometheus-additional.yaml --namespace monitoring
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm install prometheus prometheus-community/kube-prometheus-stack -f "values.yaml" --namespace monitoring
